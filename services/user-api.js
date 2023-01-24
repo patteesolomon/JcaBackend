63a2e8cedc963531683730e49260bfc4f990eec8
patteesolomon <patteesolomon@gmail.com>
2023-01-24 16:12:25 -0600

    SO BREAK THROUGH IT ALL

--- services/user-api.js
+++ services/user-api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // .then(response => response.json())
 // .then(data => console.log(data))
 // SHOW ALL 
-const baseURL = 'http://localhost:3001/user';
+const baseURL = 'https://jcausersbackend.onrender.com';
 // port to our express app 
 // defining it here makes things easier
 // the slash is for everything comming from the default route
--- utilities/data.js
+++ utilities/data.js
@@ -1,7 +1,8 @@
 
 const userData = [
-    {username: 'patteesolomon@gmail.com',
-    password: 'oneokrock5',
+    {
+      username: 'patteesolomon@gmail.com',
+      password: 'oneokrock5',
       cards: ['Software_Engineer', 'Artist']
     }
 ];
