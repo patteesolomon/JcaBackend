5c7ad710dbf6244dd5a8eecc65da09288c352505
patteesolomon <patteesolomon@gmail.com>
2023-01-24 15:50:11 -0600

    NO MORE COMPROMISE THIS IS DO OR DIE ILL WARN YOU ONE LAST TIME

--- utilities/cardData.js
+++
@@ -1,9 +0,0 @@
-// this data actually needs to be 
-// fed into the data base 
-const cardData = [
-    {
-        title: 'Software_Engineer',
-        description: 'an Engineer that creates different form of software.',
-        Image: ''
-    }
-];
