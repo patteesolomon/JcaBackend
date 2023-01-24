c17b2a6ebb51c882433f5bd23234399655a400f0
patteesolomon <patteesolomon@gmail.com>
2023-01-24 17:02:04 -0600

    names

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../models/Users.js');
+const Users = require('../models/user.js');
 // Remember INDUCES
 
 // Index
