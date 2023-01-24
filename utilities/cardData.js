733f9c743e7db0efac6594ddd42fbf2a173feac3
patteesolomon <patteesolomon@gmail.com>
2023-01-24 13:55:30 -0600

    better

--- utilities/cardData.js
+++ utilities/cardData.js
@@ -1,7 +1,9 @@
 // this data actually needs to be 
 // fed into the data base 
 const cardData = [
-    {title: 'Software_Engineer',
+    {
+        title: 'Software_Engineer',
         description: 'an Engineer that creates different form of software.',
-        Image:''}
+        Image: ''
+    }
 ];
